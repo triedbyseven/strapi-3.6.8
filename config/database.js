d0e429c6b0fde4f1b30e064daa1402c8bb544dce
Michael Camacho <mikedev0431@gmail.com>
2021-11-16 14:59:03 -0800

    Removed hard coded credentails from all database and plugin config files.

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'thedivinemedspa-db.cn54kpapt4jy.us-west-1.rds.amazonaws.com'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapiheadlessdb'),
-        username: env('DATABASE_USERNAME', 'main'),
-        password: env('DATABASE_PASSWORD', 'Xh~b`=Db7X9;XeVw'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
       options: {}
     },
